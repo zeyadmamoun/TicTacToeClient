33017868724ddaca02dd71e8fddcbf0c1b3efc56
mahmoud126d <mahmouud.salah2@gmail.com>
2025-01-10 19:53:44 +0200

    handle players list

--- src/network/Client.java
+++ src/network/Client.java
@@ -10,9 +10,11 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import screens.LoginScreenFXMLController;
 
@@ -20,70 +22,90 @@ import screens.LoginScreenFXMLController;
  *
  * @author zeyad_maamoun
  */
-public class Client extends Thread{
-    
+public class Client extends Thread {
+
     Socket soc;
     DataInputStream ear;
     DataOutputStream mouth;
     JSONObject obj;
-    boolean haveAccess = false;
+    private boolean haveAccess = false;
     private LoginUiHandler loginHandler;
-    
+    private DashboadrdUiHandler dashboadrdUiHandler;
     //private instance so no one can accesss it directly
     private static Client instance;
-    
+
     // private constructor so no one can make any new instance from this class.
     private Client() {
         try {
-            soc = new Socket("127.0.0.1",5005);
+            soc = new Socket("192.168.1.4", 5005);
             ear = new DataInputStream(soc.getInputStream());
             mouth = new DataOutputStream(soc.getOutputStream());
             start();
         } catch (IOException ex) {
             Logger.getLogger(LoginScreenFXMLController.class.getName()).log(Level.SEVERE, null, ex);
-        }  
+        }
     }
-    
+
     // function to get the single instance of client.
-    static public synchronized Client getInstance(){
+    static public synchronized Client getInstance() {
         if (instance == null) {
             instance = new Client();
         }
         return instance;
     }
-    
+
     public void setLoginHandler(LoginUiHandler handler) {
         this.loginHandler = handler;
+    }
+
+    public void setDashboradHandler(DashboadrdUiHandler handler) {
+        this.dashboadrdUiHandler = handler;
     }
 
     @Override
     public void run() {
         try {
-            while(true){
+            while (true) {
                 String msg = ear.readUTF();
                 obj = new JSONObject(msg);
                 String command = obj.getString("command");
                 int result = 0;
-                
-                switch(command){
-                    case "login_response" :
+
+                switch (command) {
+                    case "login_response":
                         result = obj.getInt("status");
-                        if(result == 0)
+                        if (result == 0) {
                             System.out.println("user not found");
-                        else
+                        } else {
                             System.out.println("login successfull");
-                            if (loginHandler != null) {
-                                Platform.runLater(() -> {
-                                    loginHandler.loginSuccess();
-                                });
-                            }
+                        }
+                        if (loginHandler != null) {
+                            Platform.runLater(() -> {
+                                loginHandler.loginSuccess();
+                            });
+                        }
                         break;
-                    case "register_response" :
+                    case "register_response":
                         result = obj.getInt("status");
-                        if(result == 0)
+                        if (result == 0) {
                             System.out.println("registeration failed");
-                        else
+                        } else {
                             System.out.println("registeration successfull");
+                        }
+                        break;
+                    case "players_list":
+                        if (dashboadrdUiHandler != null) {
+                            System.out.println(obj);
+                            JSONArray jsonArray = obj.getJSONArray("list");
+                            ArrayList<String> players = new ArrayList<>();
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                String element = jsonArray.getString(i);
+                                players.add(element);
+                            }
+                            Platform.runLater(() -> {
+                                dashboadrdUiHandler.updatePlayerList(players);
+                            });
+                        }
                         break;
                 }
             }
@@ -91,30 +113,34 @@ public class Client extends Thread{
             Logger.getLogger(LoginScreenFXMLController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void sendLoginCredientials(String username,String password) throws IOException{
+
+    public void sendLoginCredientials(String username, String password) throws IOException {
         JSONObject obj = new JSONObject();
         obj.put("command", "login");
         obj.put("username", username);
         obj.put("password", password);
         mouth.writeUTF(obj.toString());
     }
-    
-    public void sendRegisterCredientials(String username,String password) throws IOException{
+
+    public void sendRegisterCredientials(String username, String password) throws IOException {
         JSONObject obj = new JSONObject();
         obj.put("command", "register");
         obj.put("username", username);
         obj.put("password", password);
         mouth.writeUTF(obj.toString());
     }
-    
-    public interface LoginUiHandler{
+
+    public interface LoginUiHandler {
+
         void loginSuccess();
     }
-    
+
     interface RegisterUIHandler {
-    
-        
+
     }
-    
+
+    public interface DashboadrdUiHandler {
+
+        void updatePlayerList(ArrayList<String> players);
+    }
 }
--- src/screens/DashBoardScreenController.java
+++ src/screens/DashBoardScreenController.java
@@ -6,22 +6,42 @@
 package screens;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+import network.Client;
 
 /**
  * FXML Controller class
  *
  * @author zeyad_maamoun
  */
-public class DashBoardScreenController implements Initializable {
+public class DashBoardScreenController implements Initializable, Client.DashboadrdUiHandler {
+
+    Client client;
+
+    @FXML
+    private ListView<String> playersList;
+    @FXML
+    private Text mainHeader;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        client = Client.getInstance();
+        client.setDashboradHandler(this);
+    }
+
+    @Override
+    public void updatePlayerList(ArrayList<String> players) {
+        playersList.getItems().clear();
+        playersList.getItems().addAll(players);
+        
+    }
+
 }
