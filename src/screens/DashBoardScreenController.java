b771c9f93f90e688cd53d8326e75224f6c467b4c
suhailafarahat <suhailafarahat8@gmail.com>
2025-01-12 17:10:41 +0200

    add requests handling

--- src/screens/DashBoardScreenController.java
+++ src/screens/DashBoardScreenController.java
@@ -5,25 +5,19 @@
  */
 package screens;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
-import javafx.stage.Popup;
 import network.Client;
 
 /**
