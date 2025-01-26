3f7b73392e162edf56fd8ee7466baff18218d45b
NadiaAhmed <nadianody825@gmail.com>
2025-01-26 14:03:43 +0200

    resolve hard laevel

--- src/network/Client.java
+++ src/network/Client.java
@@ -265,8 +265,8 @@ public class Client extends Thread {
             return;
         }
         try {
-            //soc = new Socket("192.168.1.4", 5005);
-            soc = new Socket("127.0.0.1", 5005);
+            soc = new Socket("10.178.240.129", 5005);
+           // soc = new Socket("127.0.0.1", 5005);
             ear = new DataInputStream(soc.getInputStream());
             mouth = new DataOutputStream(soc.getOutputStream());
             start();
